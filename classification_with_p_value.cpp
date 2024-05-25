923d3472dacafaee836698513d368e5630912263
xiaoxiazhang00 <xiaoxiazhang00@gmail.com>
2024-05-25 10:24:39 -0700

    update functions
    1. one minus the original functions
    2. negative the functions after derivative

--- classification_with_p_value.cpp
+++ classification_with_p_value.cpp
@@ -12,11 +12,11 @@ double sech(double x) {
 void function_cx_1_func(const real_1d_array &c, const real_1d_array &x, double &func, void *ptr)
 {
     // This calculates 1/(1 + e^-(c * (x - a))
-    func = 1.0 / (1.0 + exp(-c[0] * (x[0]-c[1])));
+    func = 1 - (1.0 / (1.0 + exp(-c[0] * (x[0]-c[1]))));
 }
 void function_cx_1_grad(const real_1d_array &c, const real_1d_array &x, double &func, real_1d_array &grad, void *ptr) {
-    grad[0] = ((x[0]-c[1]) * exp(c[0] * (c[1] - x[0]))) / (exp(c[0] * (c[1] - x[0])) + 1) * (exp(c[0] * (c[1] - x[0])) + 1);
-    grad[1] = -(c[0] * exp(c[0] * (c[1] - x[0])) / (exp(c[0] * (c[1] - x[0])) + 1) * (exp(c[0] * (c[1] - x[0]))));
+    grad[0] = - (((x[0]-c[1]) * exp(c[0] * (c[1] - x[0]))) / (exp(c[0] * (c[1] - x[0])) + 1) * (exp(c[0] * (c[1] - x[0])) + 1));
+    grad[1] = c[0] * exp(c[0] * (c[1] - x[0])) / (exp(c[0] * (c[1] - x[0])) + 1) * (exp(c[0] * (c[1] - x[0])));
 }
 
 // b.Hyperbolic tangent
@@ -24,38 +24,38 @@ void Hyperbolic_tangent_func(const real_1d_array &c, const real_1d_array &x, dou
 {
     // This calculates (exp(c*(x-a)) - exp(-c*(x-a)))/((exp(c*(x-a)) + exp(-c*(x-a))))
     // x = c[0] * (x[0] - c[1])
-    func = ((exp(c[0] * (x[0] - c[1])) - exp(-c[0] * (x[0] - c[1])))/(exp(c[0] * (x[0] - c[1])) + exp(-c[0] * (x[0] - c[1]))) + 1) / 2;
+    func = 1 - (((exp(c[0] * (x[0] - c[1])) - exp(-c[0] * (x[0] - c[1])))/(exp(c[0] * (x[0] - c[1])) + exp(-c[0] * (x[0] - c[1]))) + 1) / 2);
 }
 
 void function_cx_1_grad(const real_1d_array &c, const real_1d_array &x, double &func, real_1d_array &grad, void *ptr)
 {
-    grad[0] = (2 * (x[0] - c[1]) * exp(2 * c[0] * (x[0] - c[1]))) / ((exp(2 * c[0] * (x[0] - c[1])) + 1) *  (exp(2 * c[0] * (x[0] - c[1])) + 1));
-    grad[1] = -((2 * c[0] * exp(2 * c[0] * (x[0] - a[0]))) / ((exp(2 * c[0] * (x[0] - a[0])) + 1) * (exp(2 * c[0] * (x[0] - a[0])) + 1)));
+    grad[0] = - (2 * (x[0] - c[1]) * exp(2 * c[0] * (x[0] - c[1]))) / ((exp(2 * c[0] * (x[0] - c[1])) + 1) *  (exp(2 * c[0] * (x[0] - c[1])) + 1));
+    grad[1] = (2 * c[0] * exp(2 * c[0] * (x[0] - a[0]))) / ((exp(2 * c[0] * (x[0] - a[0])) + 1) * (exp(2 * c[0] * (x[0] - a[0])) + 1));
 }
 
 // c.Arctangent function
 void function_cx_1_func(const real_1d_array &c, const real_1d_array &x, double &func, void *ptr)
 {
     // this calculates (arctan(c[0] * (x[0] - c[1]) + 1 ) / 2;
-    func = (atan(c[0] * (x[0] - c[1])) + 1) / 2;
+    func = 1 - ((atan(c[0] * (x[0] - c[1])) + 1) / 2);
 }
 
 void function_cx_1_grad(const real_1d_array &c, const real_1d_array &x, double &func, real_1d_array &grad, void *ptr)
 {
-    grad[0] = (x[0] - c[1]) / (2 * (c[0] * c[0] * (x[0] - c[1]) * (x[0] - c[1]) + 1));
-    grad[1] = c[0] / (2 * (c[0] * c[0] * (x[0] - c[1]) * (x[0] - c[1]) + 1));
+    grad[0] = - ((x[0] - c[1]) / (2 * (c[0] * c[0] * (x[0] - c[1]) * (x[0] - c[1]) + 1)));
+    grad[1] =  - (c[0] / (2 * (c[0] * c[0] * (x[0] - c[1]) * (x[0] - c[1]) + 1)));
 }
 
 // d.Gudermannian function
 void function_cx_1_func(const real_1d_array &c, const real_1d_array &x, double &func, void *ptr)
 {
-    func = (2* atan(tanh(c[0] * (x[0] - c[1])/ 2))) + 1) / 2;
+    func = 1 - ((2* atan(tanh(c[0] * (x[0] - c[1])/ 2))) + 1) / 2);
 }
 
 void function_cx_1_grad(const real_1d_array &c, const real_1d_array &x, double &func, real_1d_array &grad, void *ptr)
 {
-    grad[0] = ((x[0] - c[1]) * sech(1/2 * c[0] * (x[0] - c[1])) * sech(1/2 * c[0] * (x[0] - c[1])))/ (2 * ((tanh(1/2 * c[0] * (x[0] - c[1])) * tanh(1/2 * c[0] * (x[0] - c[1])) + 1));
-    grad[1] = -(c[0] * sech(1/2 * c[0] * (x[0] - c[1])) * sech(1/2 * c[0] * (x[0] - c[1])) / (2 * (tanh(1/2 * c[0] * (x[0] - c[1])) * tanh(1/2 * c[0] * (x[0] - c[1])) + 1)));
+    grad[0] = -(((x[0] - c[1]) * sech(1/2 * c[0] * (x[0] - c[1])) * sech(1/2 * c[0] * (x[0] - c[1])))/ (2 * ((tanh(1/2 * c[0] * (x[0] - c[1])) * tanh(1/2 * c[0] * (x[0] - c[1])) + 1)));
+    grad[1] = c[0] * sech(1/2 * c[0] * (x[0] - c[1])) * sech(1/2 * c[0] * (x[0] - c[1])) / (2 * (tanh(1/2 * c[0] * (x[0] - c[1])) * tanh(1/2 * c[0] * (x[0] - c[1])) + 1));
 }
 
 // e.Error function //Todo
@@ -80,11 +80,11 @@ void function_cx_1_grad(const real_1d_array &c, const real_1d_array &x, double &
 void function_cx_1_func(const real_1d_array &c, const real_1d_array &x, double &func, void *ptr)
 {
     // This calculate c(x-a)/(sqrt(1 + (c(x-a)^2));
-    func = ((c[0] * (x[0] - c[1]) / sqrt(1 + x[0] * x[0])) + 1) / 2;
+    func = 1 - (((c[0] * (x[0] - c[1]) / sqrt(1 + x[0] * x[0])) + 1) / 2);
 }
 
 void function_cx_1_grad(const real_1d_array &c, const real_1d_array &x, double &func, real_1d_array &grad, void *ptr)
 {
-    grad[0] = (x[0] - c[1]) / (2 * (sqrt((c[0] * c[0] * (x[0] - c[1]) * (x[0] - c[1]) + 1) * (c[0] * c[0] * (x[0] - c[1]) * (x[0] - c[1]) + 1) * (c[0] * c[0] * (x[0] - c[1]) * (x[0] - c[1]) + 1))));
-    grad[1] = c[0] / (2 * (sqrt((c[0] * c[0] * (x[0] - c[1]) * (x[0] - c[1]) + 1) * (c[0] * c[0] * (x[0] - c[1]) * (x[0] - c[1]) + 1) * (c[0] * c[0] * (x[0] - c[1]) * (x[0] - c[1]) + 1))));
+    grad[0] = - ((x[0] - c[1]) / (2 * (sqrt((c[0] * c[0] * (x[0] - c[1]) * (x[0] - c[1]) + 1) * (c[0] * c[0] * (x[0] - c[1]) * (x[0] - c[1]) + 1) * (c[0] * c[0] * (x[0] - c[1]) * (x[0] - c[1]) + 1)))));
+    grad[1] = - (c[0] / (2 * (sqrt((c[0] * c[0] * (x[0] - c[1]) * (x[0] - c[1]) + 1) * (c[0] * c[0] * (x[0] - c[1]) * (x[0] - c[1]) + 1) * (c[0] * c[0] * (x[0] - c[1]) * (x[0] - c[1]) + 1)))));
 }
