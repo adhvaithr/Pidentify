2c7953537f146ce018fb1654fddcd28b7d126cc1
Adhvaith Ravi <adhvaithr@dhcp-v018-027.mobile.uci.edu>
2024-10-22 16:41:23 -0700

    Added extensive documentation and type annotation to the functions. Also changed the class column to be in the first column instead of the last

--- UCI_ml_copy.py
+++ UCI_ml_copy.py
@@ -21,6 +21,7 @@ class DataFile:
     include_header: bool
     delimiter: str = None
 
+
 def print_help():
     print("""python UCI_ml_copy.py [-H] include_header class_col ignore_cols training input_file(s)
     \t-H\t\tOptional. Prints the usage statement. All other arguments are ignored.
@@ -29,6 +30,7 @@ def print_help():
     \tignore_col(s)\tInteger(s). A list of indexes seperated by comma. Ex. '0,1,2,3,4,5,6'.
     \ttraining\tInteger. If 1, following files are training datasets. If 0, following files are testing datasets.
     \tinput_file(s)\tFile paths. The remaining arguments are paths to files. Invalid file paths are ignored.""")
+
 
 def check_arguments(argument_list: list):
     if (len(argument_list) == 1):
@@ -44,8 +46,15 @@ def check_arguments(argument_list: list):
             print_help()
             sys.exit(1)
 
-def process_arguments():
-    """Processes command line arguments into list of Datafile objects"""
+
+def process_arguments() -> DataFile:
+    """
+    Processes command line arguments into list of Datafile objects
+    
+    :returns: DataFile object with all arguments to be used in later commands
+    :rtype: DataFile
+    """
+
     args = sys.argv
     check_arguments(args)
     include_header = bool(int(args[1]))
@@ -56,25 +65,39 @@ def process_arguments():
 
     extensions = list(map(get_file_extension, input_files))
 
-    datafile_list = [DataFile(file_path=input_files[i], file_type=extensions[i], class_col_index=class_col,\
-                               ignore_cols=ignore_cols, is_training=is_training, include_header=include_header)\
-                                  for i in range(len(input_files))]
+    datafile_list = [DataFile(file_path=input_files[i], file_type=extensions[i], class_col_index=class_col,
+                              ignore_cols=ignore_cols, is_training=is_training, include_header=include_header)
+                     for i in range(len(input_files))]
     return datafile_list
 
-def get_file_extension(file_path: str):
-    """Returns file extension of said file, taking the file path as an argument"""
+
+def get_file_extension(file_path: str) -> str:
+    """
+    Returns file extension of said file, taking the file path as an argument
+
+    :param str file_path: path of a file
+    :returns: file extension
+    :rtype: str
+    """
     try:
         return os.path.splitext(file_path)[-1]
     except ValueError:
         print("Error: Function argument must be a file path. Retry again.")
 
-def read_file(file_obj: DataFile):
-    """Reads file depending on the file_type of the object and returns DataFrame with data once done reading file"""
+
+def read_file(file_obj: DataFile) -> pd.DataFrame:
+    """
+    Reads file depending on the file_type of the object and returns DataFrame with data once done reading file.
+    
+    :param DataFile file_obj: file object of DataFile class
+    :returns: Dataframe read by Pandas depending on extension of file.
+    :rtype: pd.DataFrame
+    """
     try:
         if file_obj.file_type in [".csv", ".data"]:
             return process_header(file_obj, pd.read_csv(file_obj.file_path, header=None, sep=None, engine='python'))
         elif file_obj.file_type in [".xlsx"]:
-            #TODO: Make sure headers work properly with Excel files.
+            # TODO: Make sure headers work properly with Excel files.
             return process_header(file_obj, pd.read_excel(file_obj.file_path))
         else:
             # Confirmed to work with .txt, .arff, and .dat file extensions
@@ -82,8 +105,17 @@ def read_file(file_obj: DataFile):
     except pd.errors.ParserError as e:
         return e
 
+
 def process_header(file_obj: DataFile, df: pd.DataFrame) -> pd.DataFrame:
-    """Add a header if one is needed, or drop the header if it is present and not needed."""
+    """
+    Add a header if one is needed, or drop the header if it is present and not needed.
+
+    :param DataFile file_obj: file_obj (checks if header is present)
+    :param pd.DataFrame df: dataset that might require a header
+    :returns: formatted dataframe with header (if needed)
+    :rtype: pd.DataFrame
+    """
+
     has_header = False
     non_num_vals = 0
     for i, entry in enumerate(df.iloc[0]):
@@ -108,18 +140,28 @@ def process_header(file_obj: DataFile, df: pd.DataFrame) -> pd.DataFrame:
     elif not has_header and file_obj.include_header:
         new_header = create_header(file_obj, df)
         header_map = dict(zip(range(len(df.iloc[0])), new_header))
-        df.rename(columns=header_map,  inplace=True)
-    
+        df.rename(columns=header_map, inplace=True)
+
     return df
 
+
 def create_header(file_obj: DataFile, df: pd.DataFrame) -> list[str]:
-    """Create a header for a file that does not have one.  Numerical columns are labelled col0, col1, etc.;
-    non-numerical columns are labelled nonNumColi, nonNumCol(i+1), etc. such that i is the subsequent index
-    after the last numerical column; and the class column is labelled 'className'."""
+    """
+    Create a header for a file that does not have one.  Numerical columns are labelled col0, col1, etc.;
+    non-numerical columns are labelled nonNumCol(i), nonNumCol(i+1), etc. such that i is the subsequent index
+    after the last numerical column; and the class column is labelled 'className'.
+    
+    :param DataFile file_obj: file_obj (checks if header is present)
+    :param pd.DataFrame df: dataset that might require a header
+    :returns: header columns for my file right now
+    :rtype: list[str]
+    """
+
     header = []
     num_cols_idx = 0
     non_num_cols_idx = len(df.iloc[0]) - (len(file_obj.ignore_cols))
-    non_num_cols_idx = non_num_cols_idx - 1 if file_obj.class_col_index != -1 else non_num_cols_idx
+    non_num_cols_idx = non_num_cols_idx - \
+        1 if file_obj.class_col_index != -1 else non_num_cols_idx
     for col in range(len(df.iloc[0])):
         if col == file_obj.class_col_index:
             header.append("className")
@@ -129,24 +171,33 @@ def create_header(file_obj: DataFile, df: pd.DataFrame) -> list[str]:
         else:
             header.append(f"col{num_cols_idx}")
             num_cols_idx += 1
-    
+
     return header
 
 # TODO: Put this code fragment somewhere useful
 # It gets the name of the class from the filename for a DataFile object called 'file_obj'
 # os.path.basename(file_obj.file_path).split(".")[0]
 
-def process_files(file_objs: list[DataFile]):
-    """Loads and concatenates all csv files in file_paths and returns dataframe"""
+
+def process_files(file_objs: list[DataFile]) -> pd.DataFrame:
+    """
+    Loads and concatenates all csv files in file_paths and returns dataframe
+
+    :param list file_objs: multiple data files 
+    :returns: concatenated dataset
+    :rtype: pd.DataFrame
+    """
     # TODO: clean up datasets with incorrectly formatted points
     loaded_dfs = []
     for file_obj in file_objs:
         df = read_file(file_obj)
-        df.dropna(inplace = True)
-        df.drop_duplicates(inplace = True)
-        df = move_class_to_last_column(df, file_obj.class_col_index) 
-        loaded_dfs.append(df) # reads file first, then cleans and sorts columns in dataframe, then adds dataframe to list
-    return pd.concat(loaded_dfs, ignore_index=True) # combines dataframes in list into one
+        df.dropna(inplace=True)
+        df.drop_duplicates(inplace=True)
+        df = move_class_to_first_column(df, file_obj.class_col_index)
+        # reads file first, then cleans and sorts columns in dataframe, then adds dataframe to list
+        loaded_dfs.append(df)
+    # combines dataframes in list into one
+    return pd.concat(loaded_dfs, ignore_index=True)
     # TODO: Verify that dropping duplicates will remove redundant column names when there are multiples files
     # passed at the same time and decide whether to use the commented out code below
     '''
@@ -155,15 +206,34 @@ def process_files(file_objs: list[DataFile]):
     return final_df 
     '''
 
-def move_class_to_last_column(df, class_col_ind: int):
-    """Swaps class column at index class_col_ind with the last column in the df Dataframe"""
+
+def move_class_to_first_column(df: pd.DataFrame, class_col_ind: int) -> pd.DataFrame:
+    """
+    Swaps class column at index class_col_ind with the first column in the df Dataframe
+
+    :param pd.DataFrame df: unformatted dataset
+    :param int class_col_ind: location of the class column
+    :returns: formatted dataset with class column at first column
+    :rtype: pd.DataFrame
+    """
+
     columns = list(df.columns)
-    columns[class_col_ind], columns[-1] = columns[-1], columns[class_col_ind] # moves the class column to the last column in the dataframe
+    # moves the class column to the last column in the dataframe
+    columns[class_col_ind], columns[0] = columns[0], columns[class_col_ind]
     return df[columns]
 
-def valid_file_name(desired_name: str):
-    """Check that the characters within desired name do not include special characters with ascii"""
-    valid_characters = [32, 95] + list(range(48, 58)) + list(range(65, 91)) + list(range(97, 123))
+
+def valid_file_name(desired_name: str) -> bool:
+    """
+    Check that the characters within desired name do not include special characters with ascii
+
+    :param str desired_name: tentative name of file
+    :returns: Does it include special characters or not?
+    :rtype: bool
+    """
+
+    valid_characters = [
+        32, 95] + list(range(48, 58)) + list(range(65, 91)) + list(range(97, 123))
     for char in desired_name:
         ascii_char = ord(char)
         if ascii_char not in valid_characters:
