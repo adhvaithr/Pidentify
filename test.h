8d3f2e262eec37372863b3a095ec000750c4411f
Megan Uyehara <uyeharam@uci.edu>
2025-02-16 08:23:44 -0800

    Add support for testing using default or user defined p value threshold(s)

--- test.h
+++ test.h
@@ -2,9 +2,16 @@
 #define TEST_H
 
 #include <vector>
+#include <unordered_map>
+#include <string>
+#include <limits>
 #include "classMember.h"
 
+const double PVALUE_INCREMENT = 5.0;
+const int TOTAL_DYNAMIC_PVALUES = 5;
+const std::vector<double> CONSTANT_PVALUE_THRESHOLDS = { 0.50, 0.30, 0.10, 0.05 };
+
 void kFoldSplit(std::vector<ClassMember>& dataset, std::vector<ClassMember> kSets[]);
-void test(const std::vector<ClassMember>& dataset, double pvalueThreshold, size_t fold);
+void test(const std::vector<ClassMember>& dataset, std::unordered_map<std::string, double[5]>& predictionStatistics, size_t fold, double pvalueThreshold = std::numeric_limits<double>::lowest());
 
 #endif
