aa3322bce1d5f48b15ddd11df860cc9bf79d4cff
vidhyap22 <149842879+vidhyap22@users.noreply.github.com>
2026-06-12 09:28:03 -0700

    Update logger.py
    move logging to stderr

--- UCI_ml_converter/logger.py
+++ UCI_ml_converter/logger.py
@@ -1,15 +1,20 @@
 import logging
-import os
+import sys
 
 
-def create_logger(logger_name: str, log_file_path: str) -> logging.Logger:
+def create_logger(logger_name: str) -> logging.Logger:
     """
-    Create a Logger object that outputs logs in the format: "mm/dd/yyyy hh:mm:ss A.M./P.M.: log_message".
+    Create a Logger object that outputs logs to stderr in the format: "mm/dd/yyyy hh:mm:ss A.M./P.M.: log_message".
     """
-    log_file_dir = os.path.dirname(log_file_path)
-    if not os.path.exists(log_file_dir):
-        os.makedirs(log_file_dir)
     logger = logging.getLogger(logger_name)
-    logging.basicConfig(filename=log_file_path, encoding="utf-8", format="%(asctime)s: %(message)s",
-                        datefmt="%m/%d/%Y %I:%M:%S %p", level=logging.DEBUG)
-    return logger    
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+
+    # Avoid duplicate handlers if create_logger is called more than once.
+    if not logger.handlers:
+        handler = logging.StreamHandler(sys.stderr)
+        handler.setFormatter(logging.Formatter("%(asctime)s: %(message)s",
+                                               datefmt="%m/%d/%Y %I:%M:%S %p"))
+        logger.addHandler(handler)
+
+    return logger
